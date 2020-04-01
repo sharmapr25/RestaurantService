5c03ac6b78b59632ff7ae8365f845c8efb56d540
preeti sharma <preetisharma@preetis-MacBook-Pro.local>
2020-04-01 22:16:20 +0530

    add resturants api of order to update replica in order application

--- src/main/java/food/service/demo/web/RestaurantController.java
+++ src/main/java/food/service/demo/web/RestaurantController.java
@@ -5,10 +5,17 @@ import food.service.demo.service.RestaurantService;
 import food.service.demo.web_api_contract.CreateRestaurantRequest;
 import food.service.demo.web_api_contract.CreateRestaurantResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping(path = "/restaurants")
@@ -17,9 +24,19 @@ public class RestaurantController {
     @Autowired
     RestaurantService restaurantService;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @PostMapping
     public CreateRestaurantResponse create(@RequestBody CreateRestaurantRequest request){
-        Restaurant restaurantMenu = restaurantService.create(request);
-        return new CreateRestaurantResponse(restaurantMenu.getId());
+        Restaurant restaurant = restaurantService.create(request);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<Restaurant> entity = new HttpEntity<Restaurant>(restaurant,headers);
+        restTemplate.exchange(
+                "http://localhost:8080/restaurants", HttpMethod.POST, entity, String.class).getBody();
+
+        return new CreateRestaurantResponse(restaurant.getId());
     }
 }
